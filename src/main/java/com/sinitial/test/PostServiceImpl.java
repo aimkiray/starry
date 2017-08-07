b10526576f3706de2999ab5de2df7956754900b1
nekuata <nekuata@gmail.com>
2017-08-07 18:50:00 +0800

    test git

--- src/main/java/com/sinitial/test/PostServiceImpl.java
+++ src/main/java/com/sinitial/test/PostServiceImpl.java
@@ -1,15 +1,12 @@
 package com.sinitial.test;
 
-import com.sinitial.entity.Author;
-import com.sinitial.entity.Post;
-import org.apache.ibatis.io.Resources;
+import com.sinitial.dao.PostMapper;
+import com.sinitial.domain.Post;
+import com.sinitial.domain.PostExample;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -35,27 +32,13 @@ import java.util.*;
  * ━━━━━━感觉萌萌哒━━━━━━
  */
 
-public class PostDaoImpl {
+public class PostServiceImpl {
 
-//    更改xml文件可切换数据库，可选mariadb和oracle
-    private static final String DB = "oracle";
-    private static final String MYBATIS_RESOURCE = "mybatis/"+DB+"/Configuration.xml";
+    SqlSessionFactory sqlSessionFactory = null;
+    PostMapper postMapper = null;
 
-    /**
-     * 用来获取sqlsession的通用方法
-     * @return sqlsession
-     * @throws IOException 输入输出异常，sqlsession的close方法可以关闭输入输出流，故抛出，在调用此方法的方法中关闭
-     */
-    public SqlSession getSession() throws IOException {
-
-//        打开输入输出流，sqlsession的close方法可以关闭输入输出流，故抛出，在调用此方法的方法中关闭
-        InputStream inputStream = Resources.getResourceAsStream(MYBATIS_RESOURCE);
-//        根据配置文件得到sqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-//        再根据sqlSessionFactory得到session
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-
-        return sqlSession;
+    public PostServiceImpl() {
+        sqlSessionFactory = NewSqlSessionFactory.getSessionFactory();
     }
 
     /**
@@ -63,18 +46,14 @@ public class PostDaoImpl {
      */
     @Test
     public void findAllPost() {
-        List<Post> posts = null;
-        SqlSession sqlSession = null;
-        try {
-            sqlSession = getSession();
-            posts = sqlSession.selectList("Post.findAllPost");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        postMapper = sqlSession.getMapper(PostMapper.class);
+        /*PostExample postExample = new PostExample();
+        PostExample.Criteria criteria = postExample.createCriteria();*/
+        List<Post> posts = postMapper.selectByExample(null);
             if (sqlSession != null) {
                 sqlSession.close();
             }
-        }
         for (Post post : posts) {
             System.out.println(post);
         }
@@ -83,7 +62,7 @@ public class PostDaoImpl {
     /**
      * 获取全部的文章
      */
-    @Test
+    /*@Test
     public void findPost() {
         List<Post> posts = null;
         SqlSession sqlSession = null;
@@ -105,9 +84,9 @@ public class PostDaoImpl {
         }
     }
 
-    /**
+    *//**
      * 通过文章名查找作者
-     */
+     *//*
     @Test
     public void findPostByName() {
         List<Post> posts = new ArrayList<>();
@@ -126,9 +105,9 @@ public class PostDaoImpl {
         }
     }
 
-    /**
+    *//**
      * 根据id删除文章
-     */
+     *//*
     @Test
     public void deletePost() {
         List<Post> posts = null;
@@ -136,9 +115,9 @@ public class PostDaoImpl {
         try {
             sqlSession = getSession();
             int result = sqlSession.delete("Post.deletePostById",5);
-            /*if ("Oracle".equals(DB) && result > 0) {
+            *//*if ("Oracle".equals(DB) && result > 0) {
                 sqlSession.commit();
-            }*/
+            }*//*
             sqlSession.commit();
         } catch (IOException e) {
             e.printStackTrace();
@@ -149,9 +128,9 @@ public class PostDaoImpl {
         }
     }
 
-    /**
+    *//**
      * 向数据库添加文章
-     */
+     *//*
     @Test
     public void insertPost() {
         SqlSession sqlSession = null;
@@ -161,9 +140,9 @@ public class PostDaoImpl {
             Author author = new Author(2,"博丽灵梦","1234","巫女",new Date());
             post = new Post("9の日常","publish","post",author,"提问，9是什么？","",new Date());
             int result = sqlSession.insert("Post.insertPost",post);
-            /*if ("Oracle".equals(DB) && result > 0) {
+            *//*if ("Oracle".equals(DB) && result > 0) {
                 sqlSession.commit();
-            }*/
+            }*//*
             sqlSession.commit();
 
         } catch (IOException e) {
@@ -175,9 +154,9 @@ public class PostDaoImpl {
         }
     }
 
-    /**
+    *//**
      * 向数据库添加文章
-     */
+     *//*
     @Test
     public void updatePost() {
         SqlSession sqlSession = null;
@@ -186,9 +165,9 @@ public class PostDaoImpl {
             sqlSession = getSession();
             post = new Post();
             int result = sqlSession.update("Post.updatePostId",post);
-            /*if ("Oracle".equals(DB) && result > 0) {
+            *//*if ("Oracle".equals(DB) && result > 0) {
                 sqlSession.commit();
-            }*/
+            }*//*
             sqlSession.commit();
 
         } catch (IOException e) {
@@ -198,5 +177,5 @@ public class PostDaoImpl {
                 sqlSession.close();
             }
         }
-    }
+    }*/
 }
