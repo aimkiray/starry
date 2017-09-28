ecf06b28fca83821b365b58cdf861450b2e5fb84
nekuata <nekuata@gmail.com>
2017-09-29 01:29:52 +0800

    防止XSS注入 & 自动化部署

---
+++ src/main/notebook/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/starry
+git pull
+mvn clean package
+cd /home/starry/target
+mv starry.war ROOT.war
+sh /home/apache-tomcat-9.0.0.M26/bin/shutdown.sh
+rm -rf /home/apache-tomcat-9.0.0.M26/webapps/ROOT
+mv ROOT.war /home/apache-tomcat-9.0.0.M26/webapps/
+sh /home/apache-tomcat-9.0.0.M26/bin/startup.sh
