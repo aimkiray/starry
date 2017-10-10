eab70bdc71fd62ce95c5e4f8315ef6efbd32e191
nekuata <nekuata@gmail.com>
2017-10-11 02:28:24 +0800

    自动化部署

--- src/main/notebook/deploy.sh
+++ src/main/notebook/deploy.sh
@@ -4,21 +4,32 @@ git pull
 mvn clean package
 cd /home/starry/target
 mv starry.war ROOT.war
-. /home/apache-tomcat-9.0.0.M26/bin/shutdown.sh
-rm -rf /home/headimg
-mkdir /home/headimg
-mv /home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg/* /home/headimg
+echo "关闭Tomcat"
+sh /home/apache-tomcat-9.0.0.M26/bin/shutdown.sh
+echo "转移资源"
+if test ! -d "/home/headimg"
+then
+    mkdir /home/headimg
+else
+    rm -rf /home/headimg/*
+fi
+if test -d "/home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg/"
+then
+    mv /home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg/* /home/headimg/
+fi
+echo "重新部署"
 rm -rf /home/apache-tomcat-9.0.0.M26/webapps/ROOT
 mv ROOT.war /home/apache-tomcat-9.0.0.M26/webapps/
-. /home/apache-tomcat-9.0.0.M26/bin/startup.sh
-echo "Waiting for Tomcat to start"
+echo "启动Tomcat"
+sh /home/apache-tomcat-9.0.0.M26/bin/startup.sh
+echo "Waiting for Tomcat to start..."
 while true
 do
-if test ! -d "/home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/"
-then
-mkdir /home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg
-break
-fi
+    if test -d "/home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/"
+    then
+        mkdir /home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg/
+        break
+    fi
 done
-mv /home/headimg/* /home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg
+mv /home/headimg/* /home/apache-tomcat-9.0.0.M26/webapps/ROOT/resources/headimg/
 echo -e "\033[32m Deployment is complete! \033[0m"
