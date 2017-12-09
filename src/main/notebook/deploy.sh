23dc81349bb276e5c6ac40ec4ccff487f1538aa8
nekuata <nekuata@gmail.com>
2017-12-09 20:19:55 +0800

    update > surface & deploy fix bug

--- src/main/notebook/deploy.sh
+++ src/main/notebook/deploy.sh
@@ -9,13 +9,13 @@ mv starry.war ROOT.war
 echo "关闭Tomcat"
 sh ${path}/apache-tomcat/bin/shutdown.sh
 echo "转移资源"
-if test ! -d "${path}/headimg"
+if test ! -d "$path/headimg"
 then
     mkdir ${path}/headimg
 else
     rm -rf ${path}/headimg/*
 fi
-if test -d "${path}/apache-tomcat/webapps/ROOT/resources/headimg/"
+if test -d "$path/apache-tomcat/webapps/ROOT/resources/headimg/"
 then
     mv ${path}/apache-tomcat/webapps/ROOT/resources/headimg/* ${path}/headimg/
 fi
@@ -27,15 +27,15 @@ sh ${path}/apache-tomcat/bin/startup.sh
 echo "Waiting for Tomcat ready..."
 while true
 do
-    if test -d "${path}/apache-tomcat/webapps/ROOT/resources/"
+    if test -d "$path/apache-tomcat/webapps/ROOT/resources/"
     then
         mkdir ${path}/apache-tomcat/webapps/ROOT/resources/headimg/
         break
     fi
 done
 mv ${path}/headimg/* ${path}/apache-tomcat/webapps/ROOT/resources/headimg/
-if test -d "${path}/favicon/"
+if test -d "$path/favicon/"
 then
-    mv ${path}/favicon/* ${path}/apache-tomcat/webapps/ROOT/
+    cp ${path}/favicon/* ${path}/apache-tomcat/webapps/ROOT/
 fi
 echo -e "\033[32m Deployment is complete! \033[0m"
